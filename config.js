05bf17bb64c6a11e35d1ec8b20542e0a28e9e7e1
Natalia <Natalia1992.92@mail.ru>
2016-06-27 18:51:12 +0300

    set env vars

--- config.js
+++ config.js
@@ -1,13 +1,16 @@
 var url = require('url');
 var config = {};
 
-if ( process.env.REDISCLOUD_URL ) {
+if ( process.env.NODE_ENV == 'production') {
 	var redisURL = url.parse(process.env.REDISCLOUD_URL);
+	config.db = process.env.MONGOLAB_URI;
+	config.port = process.env.PORT;
 } else {
 	var redisURL = { hostname: 'localhost', port: 6379 };
+	config.db = 'mongodb://localhost/test';
+	config.port = 8888;
 }
-config.port = 8888;
-config.db = 'mongodb://localhost/test';
+
 config.redis = {
 	host: redisURL.hostname,
 	port: redisURL.port,
--- server/controllers/msgs.js
+++ server/controllers/msgs.js
@@ -2,12 +2,13 @@ var Server   = require('../../server'),
     io       = Server.io,
     async    = require('async'),
     moment   = require('moment'),
-    //client   = require('redis').createClient(),
+    config   = require('../../config'),
+    client   = require('redis').createClient(config.redisURL.port, config.redisURL.hostname, {no_ready_check: true}),
     Msg      = require('../models/msg'),
     Dialogue = require('../models/dialogue')
     User     = require('../models/user');
 
-/*io.on('connection', function (socket) {
+io.on('connection', function (socket) {
     socket.on('add_user', function (data) {
         if ( data.id ) {
             socket.user_id = data.id;
@@ -85,7 +86,7 @@ var Server   = require('../../server'),
         }
     });
 
-});*/
+});
 
 var populate_data  = 'msgs',
     populate_fields = {
