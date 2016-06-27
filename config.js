3c276123176ae71e1ebe82c1b09ddb53c99db498
Natalia <Natalia1992.92@mail.ru>
2016-06-27 17:33:45 +0300

    test

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 var url = require('url');
 var config = {};
+console.log('MY PROCESS ENV: ');
+console.log(process.env);
 var redisURL = process.env.REDISCLOUD_URL ? url.parse(process.env.REDISCLOUD_URL) : {hostname: 'localhost', port: 6379};
 
 config.port = 8888;
